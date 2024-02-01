4d792a1e3f9e89175cbca3370542aac8afd1ec26
Mark <mark.borisov.mark@yandex.ru>
2024-02-01 23:05:09 +0300

    Создал счетчик

--- Couter/ViewController.swift
+++ Couter/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button: UIButton!
+    
+    @IBOutlet weak var counterValue: UILabel!
+    private var count: Int = 0
+    public func CounterResset() {
+    
+            count += 1
+            counterValue.text = ("Значение счетчика:\(count)")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterValue.text = "Значение cчетчика:0"
+    
         // Do any additional setup after loading the view.
+    }
+    @IBAction func ButtonA(_ sender: Any)  {
+        CounterResset()
     }
 
 
